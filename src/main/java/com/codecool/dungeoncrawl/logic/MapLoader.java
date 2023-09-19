a71d3d748016c4179b8d9cf200435c320ca09e22
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-09-19 18:10:51 +0200

    extend MapLoader

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -3,8 +3,8 @@ package com.codecool.dungeoncrawl.logic;
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
-import com.codecool.dungeoncrawl.data.actors.Player;
-import com.codecool.dungeoncrawl.data.actors.Skeleton;
+import com.codecool.dungeoncrawl.data.Specialities.*;
+import com.codecool.dungeoncrawl.data.actors.*;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -41,6 +41,34 @@ public class MapLoader {
                         case '@':
                             cell.setType(CellType.FLOOR);
                             map.setPlayer(new Player(cell));
+                            break;
+                        case 'f':
+                            cell.setType(CellType.FLOOR);
+                            new Fire(cell);
+                            break;
+                        case 'k':
+                            cell.setType(CellType.FLOOR);
+                            new Key(cell);
+                            break;
+                        case 'w':
+                            cell.setType(CellType.FLOOR);
+                            new Water(cell);
+                            break;
+                        case 'W':
+                            cell.setType(CellType.FLOOR);
+                            new WaterGate(cell);
+                            break;
+                        case 'g':
+                            cell.setType(CellType.FLOOR);
+                            new Gate(cell);
+                            break;
+                        case '1':
+                            cell.setType(CellType.FLOOR);
+                            new WaterWall(cell);
+                            break;
+                        case '2':
+                            cell.setType(CellType.FLOOR);
+                            new WaterSwitchOff(cell);
                             break;
                         default:
                             throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
