e483c646957d5a7c4d9337bc18529b244523b962
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-09-19 12:59:51 +0200

    creating isValidMove and makeRandomMove for actors

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -1,15 +1,24 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.Drawable;
+
+import java.util.Random;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
 
+    private static final int NUM_DIRECTIONS = 4;
+    private static final int[] DX = {0, 0, -1, 1};
+    private static final int[] DY = {-1, 1, 0, 0};
+    private Random random;
+
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
+        random = new Random();
     }
 
     public void move(int dx, int dy) {
@@ -33,5 +42,22 @@ public abstract class Actor implements Drawable {
 
     public int getY() {
         return cell.getY();
+    }
+
+    public void makeRandomMove(){
+        int randomDirectionX = random.nextInt(NUM_DIRECTIONS);
+        int randomDirectionY = random.nextInt(NUM_DIRECTIONS);
+
+        int dx = DX[randomDirectionX];
+        int dy = DY[randomDirectionY];
+
+        if(isValidMove(dx, dy)){
+            move(dx, dy);
+        }
+    }
+
+    public boolean isValidMove(int x, int y){
+        Cell nextCell = getCell().getNeighbor(x, y);
+        return nextCell != null && nextCell.getType() == CellType.FLOOR && nextCell.getActor() == null;
     }
 }
