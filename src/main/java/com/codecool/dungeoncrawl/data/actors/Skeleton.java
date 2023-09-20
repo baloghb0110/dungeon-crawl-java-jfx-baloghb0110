62e56c61c46ce5404c7e1c35f5a985fbd9e375e6
Balogh Barnabas <111227723+baloghb0110@users.noreply.github.com>
2023-09-20 13:19:12 +0200

    allowing skeletons to chase the player when they are nearby

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
@@ -12,4 +12,14 @@ public class Skeleton extends Actor {
         return "skeleton";
     }
 
+    public void chasePlayer(Player player) {
+        int playerX = player.getX();
+        int playerY = player.getY();
+        int dx = Integer.compare(playerX, getX());
+        int dy = Integer.compare(playerY, getY());
+
+        if (isValidMove(dx, dy)) {
+            move(dx, dy);
+        }
+    }
 }
