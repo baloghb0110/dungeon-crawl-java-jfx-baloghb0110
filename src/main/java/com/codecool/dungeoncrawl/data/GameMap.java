87c5e266748c12f9f26167414e1996589fac2bd7
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-09-20 20:48:27 +0200

    implement openWaterGate method

--- src/main/java/com/codecool/dungeoncrawl/data/GameMap.java
+++ src/main/java/com/codecool/dungeoncrawl/data/GameMap.java
@@ -1,6 +1,10 @@
 package com.codecool.dungeoncrawl.data;
 
 import com.codecool.dungeoncrawl.data.actors.Player;
+import com.codecool.dungeoncrawl.data.specialities.Gate;
+import com.codecool.dungeoncrawl.data.specialities.Speciality;
+import com.codecool.dungeoncrawl.data.specialities.Water;
+import com.codecool.dungeoncrawl.data.specialities.WaterGate;
 import com.codecool.dungeoncrawl.logic.Game;
 
 public class GameMap {
@@ -39,5 +43,44 @@ public class GameMap {
 
     public int getHeight() {
         return height;
+    }
+
+    public Cell findOpenedWaterGate() {
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                Cell cell = getCell(x, y);
+                if (cell.hasSpeciality()) {
+                    Speciality speciality = cell.getSpeciality();
+                    if (speciality.getTileName().equals("waterGateOpen")) {
+                        return cell;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void findAndOpenWaterGate() {
+        for (int x = 0; x < getWidth(); x++) {
+            for (int y = 0; y < getHeight(); y++) {
+                Cell cell = getCell(x, y);
+                if (cell.hasSpeciality()) {
+                    Speciality speciality = cell.getSpeciality();
+                    if (speciality.getTileName().equals("waterSwitchOn")) {
+                        for (int j = 0; j < getWidth(); j++) {
+                            for (int h = 0; h < getHeight(); h++) {
+                                Cell cell1 = getCell(j, h);
+                                if (cell1.hasSpeciality()) {
+                                    Speciality speciality1 = cell1.getSpeciality();
+                                    if (speciality1.getTileName().equals("waterGateClosed")) {
+                                        ((WaterGate) speciality1).setOpened(true);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
