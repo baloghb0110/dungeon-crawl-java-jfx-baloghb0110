25944b41c7d5ca6e9ae1688cae6e8b8b87befa48
Bebe <125598346+SebestyenBenedek@users.noreply.github.com>
2023-09-20 20:47:02 +0200

    implement Space class

--- src/main/java/com/codecool/dungeoncrawl/ui/keyeventhandler/Space.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/keyeventhandler/Space.java
@@ -1,6 +1,9 @@
 package com.codecool.dungeoncrawl.ui.keyeventhandler;
 
+import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.specialities.WaterSwitcher;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -9,7 +12,32 @@ public class Space implements KeyHandler {
 
     @Override
     public void perform(KeyEvent event, GameMap map) {
-        if (code.equals(event.getCode()))
-            map.getPlayer();
+        Cell playerCell = map.getPlayer().getCell();
+        Cell cellUp = playerCell.getNeighbor(0, 1);
+        Cell cellDown = playerCell.getNeighbor(0, -1);
+        Cell cellRight = playerCell.getNeighbor(1, 0);
+        Cell cellLeft = playerCell.getNeighbor(-1, 0);
+        if (code.equals(event.getCode())) {
+            if (cellUp.hasSpeciality() && cellUp.getType() == CellType.WALL) {
+                cellUp.setType(CellType.FLOOR);
+            }
+            if (cellDown.hasSpeciality() && cellDown.getType() == CellType.WALL) {
+                cellDown.setType(CellType.FLOOR);
+            }
+            if (cellRight.hasSpeciality() && cellRight.getType() == CellType.WALL) {
+                cellRight.setType(CellType.FLOOR);
+                checkSpeciality(cellRight, map);
+            }
+            if (cellLeft.hasSpeciality() && cellLeft.getType() == CellType.WALL) {
+                cellLeft.setType(CellType.FLOOR);
+            }
+        }
+    }
+
+    private void checkSpeciality(Cell neighbourCell, GameMap map) {
+        if (neighbourCell.getSpeciality().getTileName().equals("waterSwitchOff")) {
+            ((WaterSwitcher) neighbourCell.getSpeciality()).setOpened(true);
+            map.findAndOpenWaterGate();
+        }
     }
 }
